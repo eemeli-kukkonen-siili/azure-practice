afcbc43f49df0caf41e3beeb98db5a8a234902bb
Eemeli Kukkonen <eemeli.kukkonen@siili.com>
2025-12-15 17:27:17 +0200

    Initial commit

---
+++ ipcheck/Program.cs
@@ -0,0 +1,18 @@
+﻿// Check if network is available and dox yourself if it is
+if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+{
+    System.Console.WriteLine("Current IP Addresses:");
+
+    var hostname = System.Net.Dns.GetHostName();
+    System.Console.WriteLine(hostname);
+    System.Net.IPHostEntry host = System.Net.Dns.GetHostEntry(hostname);
+
+    foreach(System.Net.IPAddress address in host.AddressList)
+    {
+        System.Console.WriteLine($"\t{address}");
+    }
+}
+else
+{
+    System.Console.WriteLine("No Network Connection");
+}
