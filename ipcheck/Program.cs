435dca1b7afc7e0441c193e671f4246f8b65a754
Eemeli Kukkonen <eemeli.kukkonen@siili.com>
2025-12-15 18:10:56 +0200

    Add important clarification to console print

--- ipcheck/Program.cs
+++ ipcheck/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Check if network is available and dox yourself if it is
 if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
 {
-    System.Console.WriteLine("Current IP Addresses:");
+    System.Console.WriteLine("Current IP Addresses for this container:");
 
     var hostname = System.Net.Dns.GetHostName();
     System.Console.WriteLine(hostname);
